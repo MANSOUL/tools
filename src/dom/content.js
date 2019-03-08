94c25fb5f83e719eeb127d917258850acc6dfecc
KuangGuanghu <1073345410@qq.com>
2019-03-08 18:20:38 +0800

    text & html

--- src/dom/content.js
+++ src/dom/content.js
@@ -1,5 +1,6 @@
 /**
- * 设置节点的文本内容
+ * 设置节点的textContent属性
+ * 如果节点本来的唯一一个子节点就是文本节点，那么设置nodeValue能够提升性能
  * @param  {HTMLElement} $ele    
  * @param  {String} content
  * @return {HTMLELement}
@@ -10,9 +11,10 @@ export function text($ele, content) {
     if (
       $firstChild &&
       $firstChild === $ele.lastChild &&
-      $firstChild.nodeType === 2
+      $firstChild.nodeType === 3
     ) {
       $firstChild.nodeValue = content;
+      return;
     }
   }
 
@@ -23,7 +25,8 @@ export function text($ele, content) {
 
 let $reuseableSvgContainer = null;
 /**
- * 设置节点的内部html
+ * 设置节点的innnerHTML属性
+ * IE的SVG节点缺少没有innerHTML，所以把内容插入到一个临时节点，然后移除目标节点的子节点，最后将子节点移动到目标节点上
  * @param  {HTMLElement} $ele    
  * @param  {String} htmlString
  * @return {HTMLELement}
